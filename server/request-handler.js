0bd3eef26106dc9029b0f722ac817fa86d2778c1
smmakowski <makowski.sm@gmail.com>
2017-02-27 16:48:13 -0800

    pass more tests, 5 left to go

--- server/request-handler.js
+++ server/request-handler.js
@@ -8,9 +8,12 @@ in basic-server.js, but it won't work as is.
 You'll have to figure out a way to export this function from
 this file and include it in basic-server.js so that it actually works.
 
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
+*Hint* Cheeck out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var fileSystem = require('fs');
+var path = require('path');
+
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -33,6 +36,8 @@ var requestHandler = function(request, response) {
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  console.log(__dirname);
+  var filePath = path.join(__dirname, request.url);
 
   // The outgoing status.
   var statusCode = 200;
@@ -44,7 +49,7 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  headers['Content-Type'] = 'application/json';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
@@ -57,7 +62,16 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Suck it world!!');
+  response.end(JSON.stringify({results: [
+    {
+      createdAt: '2017-02-27T23:49:53.511Z',
+      objectId: 'JbwGaKUfUk',
+      roomname: 'lobby',
+      text: 'fsadfdsa',
+      username: 'apple',
+      updatedAt: '2017-02-27T23:49:53.511Z'
+    }
+  ]}));
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
